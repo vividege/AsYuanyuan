9543590ddd167759ad1a9786cd3957f12c8f8b1c
vividege <xiaohailin136@gmail.com>
2020-01-14 16:04:30 +0800

    Add another solution which uses stack

--- LC-Puzzles/32-LongestValidParentheses.py
+++ LC-Puzzles/32-LongestValidParentheses.py
@@ -1,5 +1,6 @@
 def longestValidParentheses(s: str) -> int:
     '''
+    Solution I
     算法解释：
     1.从左向右扫描，如果左括号数等于右括号数，那么计数加一
     2.如果右括号数大于左括号数，则复位
@@ -28,6 +29,27 @@ def longestValidParentheses(s: str) -> int:
         elif left > right:
             left = right = 0
     return max
+
+
+def longestValidParentheses2(s: str) -> int:
+    '''
+    Solution II:
+    '''
+    max = 0
+    stack = list()
+    stack.append(-1)  # 设定上一次括号配对正确的位置为-1
+
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+            if len(stack) == 0:
+                stack.append(i)
+            else:
+                lastValid = stack[len(stack) - 1]  # lastValid为上一次括号匹配正确的位置
+                max = max if i - lastValid < max else i - lastValid  # 当前序号减去上次次匹配正确的位置即为当前最长度
+    return max
 if __name__ == '__main__':
-    s = '()(()'
-    print(longestValidParentheses(s))
+    s = ')()(())()'
+    print(longestValidParentheses2(s))
